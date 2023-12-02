bec51acc3a6a50b9f354d9db4905aeb7badea1e6
DevMoath0 <devmoath1@gmail.com>
2023-12-02 19:16:31 +0300

    Added some constants and the main buttons

---
+++ lib/components/bottom_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/constants.dart';
+
+class BottomButton extends StatelessWidget {
+  BottomButton({required this.onTap, required this.buttonTitle});
+
+  //I implemented onTap so it worked with the new nullSafety
+  final void Function() onTap;
+  final String buttonTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            buttonTitle,
+            style: kLargeButtonTextStyle,
+          ),
+        ),
+        color: kBottomContainerColour,
+        margin: const EdgeInsets.only(top: 10.0),
+        padding: const EdgeInsets.only(bottom: 20.0),
+        width: double.infinity,
+        height: kBottomContainerHeight,
+      ),
+    );
+  }
+}
